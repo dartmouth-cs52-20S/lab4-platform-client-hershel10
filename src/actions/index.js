d0b044a1520b3c1282e05e3313342f8d52f7c767
Hershel Wathore <wathoreh@gmail.com>
2020-06-08 13:46:32 -0400

    fixed issue

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const ROOT_URL = 'https://blog-platform-react.herokuapp.com/api';
-const ROOT_URL = 'http://localhost:9090/api';
+const ROOT_URL = 'https://blog-platform-react.herokuapp.com/api';
+// const ROOT_URL = 'http://localhost:9090/api';
 const API_KEY = '?key=Hershel_wathore';
 
 // keys for actiontypes
